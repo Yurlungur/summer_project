b02712180389fcf2c2123a51fa389887088314fc
Dzoenapi <jmp218@cam.ac.uk>
2017-07-07 12:26:12 -0400

    Chebyshev edge detection fixed

--- edge_detection/edgedetect.py
+++ edge_detection/edgedetect.py
@@ -15,7 +15,9 @@ from numpy.polynomial import chebyshev as T
 from scipy.integrate import quad
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
-from spectral_tools2 import VMatrix, Decompose, FilterCoeff, EdgeDetectI
+from spectral_tools2 import Vandermonde, Decompose, FilterCoeff, \
+        EdgeDetectI, EdgeDetectII, EdgeDetectIID, EdgeDetectIIC, \
+        ModalD, NodalD
 
 #############################################
 
@@ -31,7 +33,7 @@ def ConfigurePlots():
     rcParams['font.size'] = 12
     rcParams['axes.grid'] = 'True'
     rcParams['axes.titlesize'] = 15
-    rcParams['axes.labelsize'] = 20
+    rcParams['axes.labelsize'] = 15
     rcParams['axes.ymargin'] = 0.5
     rcParams['figure.figsize'] = 7, 7
     rcParams['legend.fontsize'] = 'small'
@@ -74,9 +76,9 @@ def PlotSigma(func, x, N, Vmatrix, title):
 
 ############################################
 
-def PlotEdge(func, x, title, s, N, Vmatrix):
+def PlotEdgeF(func, x, title, s, N, Vmatrix):
 
-    fig, ax = plt.subplots()
+    #fig, ax = plt.subplots()
     a_n = Decompose(func, N, Vmatrix)
     af_n, sigma = FilterCoeff(N, a_n, s) 
 
@@ -87,6 +89,8 @@ def PlotEdge(func, x, title, s, N, Vmatrix):
             str('poly1'), str('poly2')])
     theta = np.zeros((len(x), N))
     theta[:,0] = np.arccos(x)
+    peak1 = np.empty(len(factors[:,0]))
+    peak2 = np.empty(len(factors[:,0]))
     
     for k in range(N):
         theta[:,k] = (k+1)*theta[:,0]
@@ -95,10 +99,10 @@ def PlotEdge(func, x, title, s, N, Vmatrix):
 
     for i in range(len(factors[:,0])):
         
-        plt.xlim(-1,1)
-        plt.ylabel('[f](x)')
-        plt.xlabel('x')
-        plt.title('%s function jump values for s=%.2f N=%.2f' %(title, s, N))
+        #plt.xlim(-1,1)
+        #plt.ylabel('[f](x)')
+        #plt.xlabel('x')
+        #plt.title('%s function jump values for s=%.2f N=%.2f' %(title, s, N))
         
         tau = factors[i,:]
         name = factor_names[i]
@@ -111,45 +115,65 @@ def PlotEdge(func, x, title, s, N, Vmatrix):
 
         maxi = np.unravel_index(np.argmax(jumpf), jumpf.shape)
         mini = np.unravel_index(np.argmin(jumpf), jumpf.shape)
+        peak1[i] = x[mini]
+        peak2[i] = x[maxi]
 
-        print('First maximum is at x=%.2f' %x[mini])
-        print('Second maximum is at x=%.2f' %x[maxi])
 
-        plt.plot(x, jump, 'r', label='raw Chebyshev')
-        plt.plot(x, jumpf, 'b', label='filtered Chebyshev s=%d' %s)
+        #print('First maximum is at x=%.2f' %x[mini])
+        #print('Second maximum is at x=%.2f' %x[maxi])
 
-        plt.legend()
-        plt.savefig('%s_edge_%s_s%d_N%d' %(title, name, s, N))
-        plt.cla()
+        #plt.plot(x, jump, 'r', label='raw Chebyshev')
+        #plt.plot(x, jumpf, 'b', label='filtered Chebyshev s=%d' %s)
 
-    plt.close()
+        #plt.legend()
+        #plt.savefig('F%s_edge_%s_s%d_N%d' %(title, name, s, N))
+        #plt.cla()
 
-    #else:
-
-     #   plt.title('Chebyshev coefficients')
-      #  a_n_idx = np.arange(len(a_n))
-       # a_n = absolute(a_n)
-        #af_n = absolute(af_n)
-        #af_n2 = absolute(af_n2)
-
-        #isodd = np.mod(a_n_idx, 2)
-
-        #a_n = np.ma.masked_where(isodd==1 , a_n) 
-        #af_n = np.ma.masked_array(af_n, a_n.mask) 
-        #af_n2 = np.ma.masked_array(af_n2, a_n.mask) 
-        #a_n_idx = np.ma.masked_array(a_n_idx, a_n.mask)
-        #a_n, af_n, af_n2, a_n_idx = a_n.compressed(), af_n.compressed(), af_n2.compressed(), a_n_idx.compressed()
-
-        #plt.semilogy(a_n_idx, a_n, '-b', label='Raw')
-        #plt.semilogy(a_n_idx, af_n, '--r', lw=3, label='s=%d filter' %s) 
-        #plt.semilogy(a_n_idx, af_n2, '--g', lw=3, label='s=8 filter')
-
-        #plt.legend(loc='lower left')
-        #plt.savefig('%s_filter_coeff_s%d_N%d' %(title, s, N))
-
-    #print('N=%d s=%d spectral decomposition plotted' %(N, s))
     #plt.close()
 
+    return peak1, peak2
+
+############################################
+
+def PlotEdgeT(func, x, title, N, c2s, modalD):
+
+    #fig, ax = plt.subplots()
+    a_n = Decompose(func, N, c2s)
+    P = np.multiply(pi, sqrt(1.0-np.power(x, 2)))
+
+    alpha = 1.0
+    dirichlet, fourier, gibbs, poly1, poly2, expon = EdgeDetectII(N, alpha)
+    factors = np.vstack((dirichlet, fourier, gibbs, poly1, poly2, expon))
+    factor_names = np.array([str('dirichlet'), str('fourier'), str('gibbs'), \
+            str('poly1'), str('poly2'), str('expon')])
+    peak1, peak2 = np.empty(len(factors[:,0])), np.empty(len(factors[:,0]))
+    a_modalD = np.dot(a_n, modalD)
+
+    for i in range(len(factors[:,0])):
+        
+        tau = factors[i,:]
+        name = factor_names[i]
+        
+        #plt.xlim(-1,1)
+        #plt.ylabel('[f](x)')
+        #plt.xlabel('x')
+        #plt.title('%s %s edge detection for N=%d' %(title, name, N))
+        
+        a_nk = np.multiply(a_modalD, tau)
+        jump = T.chebval(x, a_nk)
+        jump = np.multiply(jump, P)
+
+        maxi = np.unravel_index(np.argmax(jump), jump.shape)
+        mini = np.unravel_index(np.argmin(jump), jump.shape)
+        peak1[i] = x[mini]
+        peak2[i] = x[maxi]
+        
+        #plt.plot(x, jump)
+        #plt.savefig('TC%s_edge_%s_N%d' %(title, name, N))
+        #plt.cla()
+
+    #plt.close()
+    return peak1, peak2
 
  #############################################
 
@@ -165,14 +189,39 @@ if __name__ == "__main__":
     chi = lambda x: 1.0 if (x>=-0.5 and x<=0.5) else 0
     g = lambda x: exp(-(x-mu)**2/(2.0*(sigma_g**2))) 
     
-    x = np.arange(-1, 1, 0.01)
-    N_val = np.array([32, 64])
+    x = np.linspace(-1, 1, num=201)
     s_val = np.array([4, 6])
     
-    N=128
-    w_i, Vmatrix = VMatrix(N)
-    PlotEdge(chi, x, str('Top_hat'), 6, N, Vmatrix)
-    #PlotEdge(g, x, str('Gaussian'), 6, N, Vmatrix)
+    N_val = np.linspace(6, 94, num=45, dtype='int')
+    
+    N = 32
+    c2s, s2c = Vandermonde(N)
+    modalD = ModalD(N, c2s)
+    PlotEdgeT(chi, x, str('Top_hat'), N, c2s, modalD)
+
+    peaks1T = np.empty((len(N_val), 6))
+    peaks2T = np.empty((len(N_val), 6))
+    
+    for idx, N in enumerate(N_val):
+    
+        c2s, s2c = Vandermonde(N)
+        modalD = ModalD(N, c2s)
+        peak1T, peak2T = PlotEdgeT(chi, x, str('Top_hat'), N, c2s, modalD)
+        peaks1T[idx,:] = peak1T
+        peaks2T[idx,:] = peak2T
+
+    fig, ax = plt.subplots()
+    for idx in range(6):
+
+        plt.title('Position of 1st edge vs N filter no. %d' %idx)
+        plt.xlabel('N')
+        plt.ylabel('Edge position')
+        plt.xlim(0,100)
+        plt.ylim(-0.65, -0.35)
+        plt.scatter(N_val, peaks1T[:,idx], marker='x', c='r', edgecolor='None')
+        plt.savefig('peaks%d' %idx)
+        plt.clf()
+
     #for N in N_val:
         
     #    print('Constructing the Vandermonde matrix for N=%d' %N)
--- edge_detection/spectral_tools2.py
+++ edge_detection/spectral_tools2.py
@@ -20,44 +20,39 @@ from matplotlib import rcParams
 
 #############################################
 
-def VMatrix(N):
+def Vandermonde(N):
 
-    x_j = T.Chebyshev.basis(N+1).roots()
-    w = T.chebweight
-    Tw_i, T2w_i = np.zeros(N+1), np.zeros(N+1)
-    Tmatrix = np.empty((N+1, N+1))
-    Vmatrix = np.empty((N+1, N+1))
-    w_j = pi/(N+1)*np.ones(N+1)
+    x_j = T.Chebyshev.basis(N+1).roots()    # collocation points
+    w_j = pi/(N+1)*np.ones(N+1)             # analytical weights
+    invnorm = np.empty(N+1)                 # 1/L2norm(T_i)
+    invnorm[0] = 1.0/pi
+    invnorm[1:] = 2.0/pi
+    Tmatrix = np.empty((N+1, N+1))          # T_i(x_j) matrix
+    c2s = np.empty((N+1, N+1))              # collocation-to-spectral
     
     for n in range(N+1):
         
-        f_n = lambda x: T.Chebyshev.basis(n)(x) * w(x)
-        f2_n = lambda x: f_n(x) * T.Chebyshev.basis(n)(x)
         Tmatrix[n,:] = T.Chebyshev.basis(n)(x_j) 
-        if n < 56:
-            T2w_i[n] = 1.0/quad(f2_n, -1.0, 1.0)[0]
-        else:
-            T2w_i[n] = 1.0/np.sum(w_j[n]*np.power(Tmatrix[n,:],2))
         
-    Tmatrix_inv = np.linalg.inv(Tmatrix)
-    Vmatrix = np.multiply(np.outer(T2w_i, w_j), Tmatrix)
+    c2s = np.multiply(np.outer(invnorm, w_j), Tmatrix)
+    s2c = np.linalg.inv(c2s)                # spectral-to-collocation
 
-    return w_j, Vmatrix
+    return c2s, s2c
 
 #############################################
 
 def Decompose(func, N, Vmatrix):
     
-    x_i = T.Chebyshev.basis(N+1).roots()
-    func = np.frompyfunc(func, 1, 1)
+    x_i = T.Chebyshev.basis(N+1).roots()    # collocation points
+    func = np.frompyfunc(func, 1, 1)        # vectorising lambda func
     u_i = func(x_i)
-    a_n = np.dot(Vmatrix, u_i)
-
+    a_n = np.dot(Vmatrix, u_i)              # coefficients using
+                                            # Vandermonde matrix
     return a_n
 
 #############################################
 
-def FilterCoeff(N, a_n, s):
+def FilterCoeff(N, a_n, s):                 # exponential filtering
 
     c = 36
     eta = 1.0/float(N)*np.arange(N+1)
@@ -69,10 +64,11 @@ def FilterCoeff(N, a_n, s):
 
 #############################################
 
-def EdgeDetectI(N, alpha):
+def EdgeDetectI(N, alpha):                  # returns discrete concentration
+                                            # coefficients for Fourier expansion
 
-    # alpha parameter is only used in Fourier concentration coefficients
-    # to calculate int((sin(x)/x)*dx)
+    # alpha parameter aplies to 
+    # Fourier c.f.to calculate Si(alpha)
 
     si = lambda x: sin(x)/x
     Si_a = quad(si, 0.0, alpha)[0]
@@ -101,12 +97,157 @@ def EdgeDetectI(N, alpha):
     return dirichlet, fourier, gibbs, poly1, poly2
 
 #############################################
-#if __name__ == "__main__":
+
+def EdgeDetectII(N, alpha):             # returns discrete concentration
+                                        # coefficients for Chebyshev expansion                  
+    # alpha parameter aplies to         
+    # Fourier c.f.to calculate Si(alpha)
+
+    si = lambda x: sin(x)/x
+    Si_a = quad(si, 0.0, alpha)[0]
+    Si_p = quad(si, 0.0, pi)[0]
+
+    dx = 2*pi/(2*N+1)*np.ones(N)
+    k = np.arange(N) + 1
+   
+    dirichlet = np.divide(-pi/log(N), k)
+    dirichlet = np.insert(dirichlet, 0, 0)
+    
+    fourier = np.divide(np.multiply(-pi/Si_a, sin(np.multiply(k, alpha/N))), k)
+    fourier = np.insert(fourier, 0, 0)
+    
+    gibbs = np.divide(np.multiply(-pi/Si_p, sin(np.multiply(k, pi/N))), k)
+    gibbs = np.insert(gibbs, 0, 0)
+
+    poly1 = -pi/N*np.ones(N)
+    poly1 = np.insert(poly1, 0, 0)
+
+    poly2 = np.multiply(-2.0*pi/(N*(N+1)), k)
+    poly2 = np.insert(poly2, 0, 0)
+
+    a = 6
+    b = pi
+    e = 1.0/N
+    
+    fun = lambda x: exp(1.0/(a*x*(x-1)))
+    integr = quad(fun, e, 1-e )[0]
+    gamma = pi/integr
+    eta = k[:-1]/float(N)
+    
+    expon = -(gamma * eta * fun(eta))/k[:-1]  
+    expon = np.insert(expon, 0, 0)
+    expon = np.append(expon, 0)
+
+    return dirichlet, fourier, gibbs, poly1, poly2, expon
+
+#############################################
+
+def EdgeDetectIIC(N, alpha):             # returns discrete concentration
+                                        # coefficients
+
+    # alpha parameter aplies to 
+    # Fourier c.f.to calculate Si(alpha)
+
+    si = lambda x: sin(x)/x
+    Si_a = quad(si, 0.0, alpha)[0]
+    Si_p = quad(si, 0.0, pi)[0]
+
+    dx = 2*pi/(2*N+1)*np.ones(N)
+    k = np.arange(N) + 1
+    
+    dirichlet = -pi/log(N)*np.ones(N)
+    
+    fourier = np.multiply(-pi/Si_a, sin(np.multiply(k, alpha/N)))
+    
+    gibbs = np.multiply(-pi/Si_p, sin(np.multiply(k, pi/N)))
+
+    poly1 = np.multiply(-pi/N, k)
+
+    poly2 = np.multiply(-2.0*pi/(N*(N+1)), np.power(k, 2))
+
+    a = 6
+    b = pi
+    e = 1.0/N
+    
+    fun = lambda x: exp(1.0/(a*x*(x-1)))
+    integr = quad(fun, e, 1-e )[0]
+    gamma = pi/integr
+    
+    expon = gamma*(k/N)*fun(k/N)
+    expon[-1] = 0
+
+    return dirichlet, fourier, gibbs, poly1, poly2, expon 
+
+#############################################
+
+def EdgeDetectIID(N, alpha):                  # returns discrete concentration
+                                            # coefficients
+
+    # alpha parameter aplies to 
+    # Fourier c.f.to calculate Si(alpha)
+
+    si = lambda x: sin(x)/x
+    Si_a = quad(si, 0.0, alpha)[0]
+    Si_p = quad(si, 0.0, pi)[0]
+
+    dx = 2*pi/(2*N+1)*np.ones(N)
+    k = np.arange(N) + 1
+    
+    dirichlet = np.divide(sin(np.multiply(k, dx/2.0)), \
+            np.multiply(k, dx)*log(N)/(-2.0))
+    
+    fourier = np.multiply(np.divide(sin(np.multiply(k, dx/2.0)), \
+            np.multiply(k, dx)*Si_a/(-2.0)), \
+            sin(k*(alpha/N)))
+
+    gibbs = np.multiply(np.divide(sin(np.multiply(k, dx/2.0)), \
+            np.multiply(k, dx)*Si_p/(-2.0)), \
+            sin(k*(pi/N))) 
+    
+    poly1 = np.divide(sin(np.multiply(k, dx/2.0)), \
+            dx*N/(-2.0))
+    
+    poly2 = np.divide(sin(np.multiply(k, dx/2.0)), \
+            np.divide(dx, k)*(N*(N+1)/(-4.0*pi)))
+    
+    return dirichlet, fourier, gibbs, poly1, poly2
+
+#############################################
+
+def ModalD(N, c2s):                    # get modal differentiation matrix
+
+    x_l = T.Chebyshev.basis(N+1).roots()    # collocation points
+    T_imatrix = np.empty((N+1, N+1))        # matrix on T' evaluated at T collocantion
+                                            # points
+    for n in range(N+1):
+        T_imatrix[:,n] = T.Chebyshev.basis(n).deriv()(x_l)
+
+    modDtrans = np.matmul(c2s, T_imatrix)
+    modalD = modDtrans.T
+
+    return modalD
+
+#############################################
+
+def NodalD(N, c2s, s2c):                    # get nodal differentiation matrix
+
+    modalD = ModalD(N, c2s, s2c)            
+    nodalD = np.linalg_multidot([s2c, modalD, c2s])
+
+    return nodalD
+
+#############################################
+
+if __name__ == "__main__":
     
     #start_time = time.time()
     
-    #N = 64
+    N = 32
+    #w_j, Vmatrix = VMatrix(N)
+    
+    ModalD(N)
 
+    sys.exit()
     #alpha = 1.0
 
     #EdgeDetectI(N, alpha)
